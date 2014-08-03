b917b8ed83db24e0c1bce4173cc7b2c5ef02f999
badrobot25 <cokehead.eu@gmail.com>
2014-08-03 22:25:10 +0300

    SimpleStringManipulation

---
+++ StringGames/Palindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringGames
+{
+    class Palindrome
+    {
+        public bool Check(string input)
+        {
+            var length = input.Length;
+            var skipMid = 1;
+            if (length % 2 == 0) skipMid = 0;
+
+            var left = input.Remove(length / 2);
+            var right = input.Remove(0, length / 2 + skipMid);
+            var reverseright = "";
+            for (var i = right.Length - 1; i >= 0; i--)
+            {
+                reverseright += right[i];
+            }
+            if (!left.Equals(reverseright)) return false;
+            return true;
+        }
+    }
+}
---
+++ StringGames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pali = new Palindrome();
+            var isPali = pali.Check("ana");
+        }
+    }
+}
