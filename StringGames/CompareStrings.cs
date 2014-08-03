e956c97af13da856eea6c54a8b0e348571f17257
badrobot25 <cokehead.eu@gmail.com>
2014-08-03 22:50:04 +0300

    Update 2

---
+++ StringGames/CompareStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringGames
+{
+    class StringWorker
+    {
+        public string GetCommonSubsequentCharacters(string a, string b)
+        {
+            string SubsOfA = a.Substring(0, (a.Length - 1 < 0) ? 0 : a.Length - 1);
+            string SubsOfB = b.Substring(0, (b.Length - 1 < 0) ? 0 : b.Length - 1);
+
+            if (a.Length == 0 || b.Length == 0)
+                return "";
+            else if (a[a.Length - 1] == b[b.Length - 1])
+                return GetCommonSubsequentCharacters(SubsOfA, SubsOfB) + a[a.Length - 1];
+            else
+            {
+                string x = GetCommonSubsequentCharacters(a, SubsOfB);
+                string y = GetCommonSubsequentCharacters(SubsOfA, b);
+                return (x.Length > y.Length) ? x : y;
+            }
+        }
+    }
+}
--- StringGames/Program.cs
+++ StringGames/Program.cs
@@ -12,12 +12,20 @@ namespace StringGames
         {
             var pali = new Palindrome();
             var isPali = pali.Check("ana");
+            Console.WriteLine(isPali ? "Palindrome = true" : "Palindrome = false");
 
             var fibbo = new Fibbo();
             var fibboNumber = fibbo.GetNthNumber("5");
-
-            Console.WriteLine(isPali ? "Palindrome = true" : "Palindrome = false");
             Console.WriteLine("Fibbo no.: " + fibboNumber);
+
+            // $string1;$string2
+            string line = "XMJYAUZ;MZJAWXU";
+            var left = line.Split(';').First();
+            var right = line.Split(';').Last();
+            var stringW = new StringWorker();
+            string[] TwoStrings = new[] { left, right };
+            Console.WriteLine(stringW.GetCommonSubsequentCharacters(left, right));
+
             Console.ReadKey();
         }
     }
